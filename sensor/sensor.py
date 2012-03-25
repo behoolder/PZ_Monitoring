9d5eef670305b3c794bfe8ba669da1306c8139dc
dakorn <dakorn@o2.pl>
2012-03-26 01:13:23 +0200

    rozbudowa sensora o system windows

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -2,6 +2,7 @@
 
 from commands import getoutput
 from system_info import SystemInfo 
+from wmi import WMI
 
 class Linux:
     """
@@ -83,7 +84,69 @@ class Linux:
         self.data.set_disk(self.disk_space())
 
         return self.data
+		
+class Windows:
+    """
+    Klasa ta zawiera szereg metod informujacych o obciazeniu systemu.
+    """
+    
+    def __init__(self):
+        self.data = SystemInfo()
 
+    def ram_usage(self):
+        """
+        Zwraca informacje na temat calkowitej, uzytej oraz wolnej ilosci
+        pamieci RAM.
+        """
+        myWMI = WMI()
+        
+        ram = {}
+
+        ram["total"] = int(myWMI.Win32_ComputerSystem()[0].TotalPhysicalMemory)
+        ram["free"]  = int(myWMI.Win32_OperatingSystem()[0].FreePhysicalMemory) * 1024
+        ram["used"]  = ram["total"] - ram["free"]
+
+        return {"RAM" : ram}
+
+    def cpu_usage(self):
+        """
+        Zwraca informacje na temat obciazenia CPU. 
+        """
+        cpu = {}
+
+        cpu["system"] = int(WMI().Win32_Processor()[0].LoadPercentage)
+
+        return {"CPU" : cpu}
+
+    def disk_space(self):
+        """
+        Zwraca informacje na temat dysku twardego.
+        """
+        myWMI = WMI()
+
+        diskList = []
+
+        for disk in myWMI.Win32_LogicalDisk(DriveType=3):
+            d = {}
+
+            d["name"]  = str(disk.Caption)
+            d["total"] = int(disk.Size)
+            d["free"]  = int(disk.FreeSpace)
+            d["used"]  = d["total"] - d["free"]
+            
+            diskList.append(d)
+
+        return {"Hard drives" : diskList}
+
+    def get_data(self):
+        """
+        Zwraca wszystkie informacje
+        """
+        self.data.set_ram(self.ram_usage())
+        self.data.set_cpu(self.cpu_usage())
+        self.data.set_disk(self.disk_space())
+
+        return self.data
 
 if __name__ == "__main__":
     pass
