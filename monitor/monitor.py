1aab634d6b8579c39cb63b95038c8416efc24a4e
Kamel Dadache <kamdad@tlen.pl>
2012-04-28 15:02:19 +0200

    system logowania

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -6,14 +6,14 @@ import sqlite3
 import os
 import mysql.connector
 from threading import Thread
-from flask import Flask, request, redirect, abort
+from flask import Flask, request, redirect, abort, session, escape
 
-class Subscription: #TODO: User
+class Subscription:
     """
     Klasa ta jest zwyklym kontenerem dla subskrypcji.
     """
 
-    def __init__(self, metric, sensor):
+    def __init__(self, user, metric, sensor):
         """
         Konstruktor klasy Subscription.
 
@@ -23,6 +23,7 @@ class Subscription: #TODO: User
         
         self.metric = metric
         self.sensor = sensor
+        self.user = user
 
     def get_metric(self):
         """
@@ -37,6 +38,13 @@ class Subscription: #TODO: User
         """
         
         return self.sensor
+
+    def get_user(self):
+        """
+        Zwraca nazwe uzytkownika na ktorego subskrypcja zostala zarejestrowana.
+        """
+        
+        return self.user
 
     
 class Monitor(Thread):
@@ -146,22 +154,23 @@ class Monitor(Thread):
 
         return self.mysql
 
-    def create_subscription(self, metric, sensor, filename = "subscriptions.db"): 
+    def create_subscription(self, user, metric, sensor, filename = "subscriptions.db"): 
         """
         Tworzy nowa subskrypcje.
 
+        user - nazwa uzytkownika
         metric - monitorowane metryki
         sensor - monitorowany sensor
         filename - nazwa pliku z baza danych
         """
 
-        self.subscriptions[max(self.subscriptions) + 1] = Subscription(metric, sensor)
+        self.subscriptions[max(self.subscriptions) + 1] = Subscription(user, metric, sensor)
 
         try: 
             con = sqlite3.connect(filename)
             cur = con.cursor()
 
-            cur.execute("INSERT INTO subscriptions VALUES (?, ?, ?, ?)", (None, 'user', str(sensor), str(metric))) #TODO: User            
+            cur.execute("INSERT INTO subscriptions VALUES (?, ?, ?, ?)", (None, user, str(sensor), str(metric))) 
             con.commit()
 
         except sqlite3.Error, e:
@@ -173,16 +182,19 @@ class Monitor(Thread):
 
         return max(self.subscriptions)
 
-    def delete_subscription(self, sid, filename = "subscriptions.db"):
+    def delete_subscription(self, user, sid, filename = "subscriptions.db"):
         """
         Usuwa subskrypcje.
 
+        user - nazwa uzytkownika
         sid - numer subskrypcji
         filename - nazwa pliku z baza danych
         """
 
         if not sid in self.subscriptions:
             raise KeyError
+        if self.subscriptions[sid].get_user() != user:
+            raise ValueError
 
         try: 
             con = sqlite3.connect(filename)
@@ -221,7 +233,7 @@ class Monitor(Thread):
             else:
                 cur.execute("SELECT * FROM subscriptions")                
                 for row in cur:
-                    self.subscriptions[row[0]] = Subscription(eval(row[3]), eval(row[2])) #TODO: User
+                    self.subscriptions[row[0]] = Subscription(row[1], eval(row[3]), eval(row[2])) 
 
         except sqlite3.Error, e:
             print "Database error: %s" % e.args[0]
@@ -230,15 +242,34 @@ class Monitor(Thread):
             if con:
                 con.close()
 
-    def get_data(self, sid): 
+    def subscription_list(self, user):
+        """
+        Zwraca liste subskrypcji dla danego uzytkownika.
+
+        user - nazwa uzytkownika
+        """
+        
+        sl = []
+        
+        for s in self.subscriptions:
+            if self.subscriptions[s].get_user() == user:
+                sl.append({"id" : s, "sensor" : self.subscriptions[s].get_sensor(), "metric" : self.subscriptions[s].get_metric()})
+
+        ret = {user : sl}
+        return str(ret)
+
+    def get_data(self, user, sid): 
         """
         Zwraca dane z sensora.
 
+        user - nazwa uzytkownika
         sid - numer subskrypcji
         """
 
         if not sid in self.subscriptions:
             raise KeyError
+        if self.subscriptions[sid].get_user() != user:
+            raise ValueError
 
         host, port = self.subscriptions[sid].get_sensor()
         ret = []
@@ -260,6 +291,8 @@ class MonitorHTTP:
 
     app = Flask("monitor")
     monitor = Monitor()
+
+    app.secret_key = os.urandom(24)
     
     def __init__(self, port):
         """
@@ -269,6 +302,20 @@ class MonitorHTTP:
         """
 
         self.port = port
+
+    @app.route('/login/', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            try:
+                session['username'] = request.form['username']
+                return redirect('/subscription_list/')
+            except Exception, e:
+                print e
+        else:
+            return '''<form action="" method="post">
+                  <p><input type=text name=username>
+                  <p><input type=submit value=Login>
+                  </form>'''
 
     @app.route('/sensors/', methods=['GET'])
     def sensors():
@@ -292,28 +339,50 @@ class MonitorHTTP:
     @app.route('/subscribe/', methods=['GET']) #TODO: POST!
     def subscribe():
         """
-        Tworzy nowa subskrypcje.
+        Tworzy nowa subskrypcje. Wymaga wczesniejszego zalogowania.
         Dostep: POST
         """
+        if 'username' in session:
+            sid = MonitorHTTP.monitor.create_subscription(session["username"], ['cpu', 'ram'], ('localhost', '5001')) #TODO: POST info
+            return redirect('/subscriptions/' + str(sid) + '/')
+        else:
+            return 'Nie jestes zalogowany!'
 
-        sid = MonitorHTTP.monitor.create_subscription(['cpu', 'ram'], ('localhost', '5001')) #TODO: POST info
-        return redirect('/subscriptions/' + str(sid) + '/')
+    @app.route('/subscription_list/', methods=['GET']) 
+    def subscription_list():
+        """
+        Zwraca liste subskrypcji. Wymaga wczesniejszego zalogowania.
+        Dostep: GET
+        """
+
+        if 'username' in session:
+            return MonitorHTTP.monitor.subscription_list(session["username"])
+
+        else:
+            return 'Nie jestes zalogowany!'
 
     @app.route('/subscriptions/<sid>/', methods=['GET', 'DELETE'])
     def subscriptions(sid):
         """
+        Wymaga wczesniejszego zalogowania.
         GET: Zwraca odpowiednie dane z sensora przypisane do konkretnej subskrypcji.
         DELETE: Usuwa subskrypcje.
 
         sid - numer subskrypcji
         """
 
+        if 'username' not in session:
+            return 'Nie jestes zalogowany!'
+
         try:
             if request.method == 'GET':
-                data = MonitorHTTP.monitor.get_data(int(sid))
+                data = MonitorHTTP.monitor.get_data(session["username"], int(sid))
             else:
-                data = MonitorHTTP.monitor.delete_subscription(sid)
-        except Exception, e:
+                data = MonitorHTTP.monitor.delete_subscription(session["username"], int(sid))
+        except ValueError, e:
+            print e
+            abort(403)
+        except KeyError, e:
             print e
             abort(404)
         else:
@@ -347,7 +416,7 @@ class MonitorHTTP:
 
         self.db_register()
 
-#        MonitorHTTP.monitor.start()
+        MonitorHTTP.monitor.start()
         MonitorHTTP.app.debug = debug
         MonitorHTTP.app.run(host = "0.0.0.0", port = self.port)
 
